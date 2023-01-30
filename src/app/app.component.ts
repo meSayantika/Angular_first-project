4990e8b2155abe7e12076670f79ba34758cd1119
Sayantika Dhar <sayantika.dhar04@gmail.com>
2023-01-30 19:04:26 +0530

    ngfor implimentation 30.1.2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,24 @@ export class AppComponent {
   bind: any;
   txt = '';
   numchk1: any;
+  show: any;
+  
   // numbers=[1,2,3,4,5,6,7,8,9,0]
   custList = [
-    { id: 1, name: 'Tanmoy Mondal', email: 'mondal.tanmoy@synergicsoftek.com' },
-    { id: 2, name: 'Raja Saha', email: 'raja@synergicsoftek.com' },
-    { id: 3, name: 'Suvrajit Banerjee', email: 'suvrajit@synergicsoftek.com' },
-    { id: 4, name: 'Lokesh Kumar Jha', email: 'lokesh@synergicsoftek.com' },
+    { id: 1, name: 'Tanmoy Mondal', email: 'mondal.tanmoy@synergicsoftek.com', age:44 },
+    { id: 2, name: 'Raja Saha', email: 'raja@synergicsoftek.com' , age:43 },
+    { id: 3, name: 'Suvrajit Banerjee', email: 'suvrajit@synergicsoftek.com' , age:42 },
+    { id: 4, name: 'Lokesh Kumar Jha', email: 'lokesh@synergicsoftek.com' , age:36 },
     {
       id: 5,
       name: 'Chittaranjan Maity',
-      email: 'chittaranjan@synergicsoftek.in',
+      email: 'chittaranjan@synergicsoftek.in', age:56
     },
-    { id: 6, name: 'Utsav Roy', email: 'utsav@synergicsoftek.com' },
-    { id: 7, name: 'Somnath Thakur', email: 'somnath@synergicsoftek.in' },
-    { id: 8, name: 'Subham Samanta', email: 'subham@synergicsoftek.in' },
-    { id: 9, name: 'Suman Mitra', email: 'suman@synergicsoftek.in' },
-    { id: 10, name: 'Sayantika Dhar', email: 'sayantika@synergicsoftek.in' },
+    { id: 6, name: 'Utsav Roy', email: 'utsav@synergicsoftek.com' , age:-86 },
+    { id: 7, name: 'Somnath Thakur', email: 'somnath@synergicsoftek.in' , age:87 },
+    { id: 8, name: 'Subham Samanta', email: 'subham@synergicsoftek.in' , age:2 },
+    { id: 9, name: 'Suman Mitra', email: 'suman@synergicsoftek.in', age:-32 },
+    { id: 10, name: 'Sayantika Dhar', email: 'sayantika@synergicsoftek.in', age:0 },
   ];
 
   // show(val:any){
@@ -43,15 +45,23 @@ export class AppComponent {
   //   console.log('hello')
   // }
 
-  show(a: any, b: any) {
-    console.log(a, b);
-    console.log(a - b);
-    if (a == '') {
-      this.numchk1 = 'enter 1st number';
-    } else if (b == '') {
-      this.numchk1 = 'enter 2nd number';
-    } else {
-      this.bind = a - b;
-    }
-  }
+//   show(a: any, b: any) {
+//     console.log(a, b);
+//     console.log(a - b);
+//     if (a == '') {
+//       this.numchk1 = 'enter 1st number';
+//     } else if (b == '') {
+//       this.numchk1 = 'enter 2nd number';
+//     } else {
+//       this.bind = a - b;
+//     }
+//   }
+edit(dt:any){
+console.log(dt);
+let index= this.custList.findIndex((cust:any)=>cust.id==dt)
+console.log(this.custList[index]);
+this.show = (this.custList[index])
+
+}
+
 }
